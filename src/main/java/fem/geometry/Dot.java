80804d0c308aeb555e12a14c35d65f2300111400
major-kolz <major-kolz@github.com>
2015-10-05 16:20:23 +0300

    Stylistic and fix inner-contour diffuse problem
    - Add constructor to Dot-based constructor, rewrite getArea and getMaxAngleIndex methods
    - Avoid problem with inner-contour diffusion (inner is Figure, outer -
      Air).

--- src/main/java/fem/geometry/Dot.java
+++ src/main/java/fem/geometry/Dot.java
@@ -26,7 +26,7 @@ public class Dot {
 	public Dot(double x_, double y_) {
 		x = x_;
 		y = y_;
-		material = DotMaterial.FIGURE;
+		material = DotMaterial.AIR;
 	}
 	public Dot(double x_, double y_, DotMaterial material_) {
 		this(x_, y_);
